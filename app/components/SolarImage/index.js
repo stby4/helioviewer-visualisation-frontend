803573fa8d9710f3f1b2eb78d428ea1336fd6b5a
Hinrich Kaestner <hinrich.kaestner@gmail.com>
2018-04-17 09:51:33 +0200

    add "View on helioviewer.org" button

--- app/components/SolarImage/index.js
+++ app/components/SolarImage/index.js
@@ -2,7 +2,7 @@ import style from './style'
 
 const SolarImagePreview = (timestamp, imageURL) =>
     `<div id="${style.previewContainer}"><img src="${imageURL}" id="${
-        style.solarImage
-    }" alt="Solar Image Preview"><div class="${style.time}">${timestamp}</div></div>`
+    style.solarImage
+    }" alt="Solar Image Preview"><div class="${style.time}">${timestamp}</div><div><a class="${style.button}" href="https://helioviewer.org/" target="_blank">View on helioviewer.org</a></div></div>`
 
 export default SolarImagePreview
--- app/components/Timeline/Chart.js
+++ app/components/Timeline/Chart.js
@@ -18,193 +18,201 @@ const labelFontSize = '11pt'
 const labelColor = '#E0E0E3'
 
 const Chart = container => {
-    return timelineData('2017-01-01:00:00:00', '2018-01-01:00:00:00').then(data => Highcharts.chart(container, {
-        chart: {
-            height: 300,
-            zoomType: 'x',
-            marginLeft: 150,
-            resetZoomButton: {
-                position: {
-                    /*
+    return timelineData('2017-01-01:00:00:00', '2018-04-12:00:00:00').then(data =>
+        Highcharts.chart(container, {
+            chart: {
+                height: 300,
+                zoomType: 'x',
+                marginLeft: 150,
+                resetZoomButton: {
+                    position: {
+                        /*
                      * align: 'right', // by default
                      * verticalAlign: 'top', // by default
                      */
-                    x: -10,
-                    y: -40,
-                },
-                theme: {
-                    fill: 'white',
-                    r: 0,
-                    // text: 'Zurücksetzen',
-                    states: {
-                        hover: {
-                            fill: 'grey',
-                            style: {
-                                color: 'white',
+                        x: -10,
+                        y: -40,
+                    },
+                    theme: {
+                        fill: 'white',
+                        r: 0,
+                        // text: 'Zurücksetzen',
+                        states: {
+                            hover: {
+                                fill: 'grey',
+                                style: {
+                                    color: 'white',
+                                },
                             },
                         },
                     },
                 },
+
+                events: {
+                    click(event) {
+                        cursor: 'pointer',
+                        alert (
+                            'Date: ' + Highcharts.dateFormat('%Y %m %d %H %M %S \n', event.xAxis[0].value) + 'Value: ' + event.yAxis[0].value
+                        );
+                    }
+                },
             },
-            /*
-             *             events: {
-             * click: function(event) {
-             * cursor: 'pointer',
-             * alert (
-             * 'Date: ' + Highcharts.dateFormat('%Y %m %d %H %M %S \n', event.xAxis[0].value) + 'Value: ' + event.yAxis[0].value
-             * );
-             * }
-             * } 
+            xAxis: {
+                type: 'datetime',
+                dateTimeLabelFormats: {
+                    /*
+             *  millisecond: '%H:%M:%S.%L',
+             * second: '%H:%M:%S',
+             * minute: '%H:%M',
+             * hour: '%H:%M',
+             * day: '%d    .%m.%Y',
+             * week: '%m.%Y',
+             * month: '%m\'%Y',
+             *year: '%Y' 
              */
-        },
-        xAxis: {
-            type: 'datetime',
-            dateTimeLabelFormats: {
-            /*  millisecond: '%H:%M:%S.%L',
-                second: '%H:%M:%S',
-                minute: '%H:%M',
-                hour: '%H:%M',
-                day: '%d    .%m.%Y',
-                week: '%m.%Y',
-                month: '%m\'%Y',
-                year: '%Y' */
+                },
+                labels: {
+                    style: {
+                        fontSize: labelFontSize,
+                    },
+                },
             },
-            labels: {
-                style: {
-                    fontSize: labelFontSize
-                }
-            }
-        },
-        title: {
-            text: 'Solar Activity Timeline',
-        },
-        yAxis: {
-            type: 'logarithmic',
-            /*
+            title: {
+                text: 'Solar Activity Timeline',
+            },
+            yAxis: {
+                type: 'logarithmic',
+                /*
              *additional horizontal dividers in chart
              *minorTickInterval: 0.1, 
              */
-            title: {
-                text: 'X-ray Flux',
-                x: 0,
-                style: {
-                    fontSize: '14pt'
-                }
+                title: {
+                    text: 'X-ray Flux',
+                    x: 0,
+                    style: {
+                        fontSize: '14pt',
+                    },
+                },
+                labels: {
+                    x: -40,
+                    style: {
+                        fontSize: labelFontSize,
+                    },
+                },
+                gridLineWidth: 0,
+                tickLength: 0,
+                plotBands: [
+                    {
+                        // A-Flare
+                        from: 0,
+                        to: 0.0000001,
+                        // zIndex: 5,
+                        color: 'rgba(40, 40, 40, 40)',
+                        borderwidth: '100',
+                        borderColor: '#FFFF',
+                        label: {
+                            text: 'A',
+                            x: -20,
+                            style: {
+                                color: labelColor,
+                                fontWeight: 'bold',
+                                fontSize: labelFontSize,
+                            },
+                        },
+                    },
+                    {
+                        // B-Flare
+                        from: 0.0000001,
+                        to: 0.000001,
+                        // zIndex: 4,
+                        color: 'rgba(0, 0, 0, 0)',
+                        label: {
+                            text: 'B',
+                            x: -45,
+                            style: {
+                                color: labelColor,
+                                fontWeight: 'bold',
+                                fontSize: labelFontSize,
+                            },
+                        },
+                    },
+                    {
+                        // C-Flare
+                        from: 0.000001,
+                        to: 0.00001,
+                        // zIndex: 5,
+                        color: 'rgba(40, 40, 40, 40)',
+                        label: {
+                            text: 'C',
+                            x: -45,
+                            style: {
+                                color: labelColor,
+                                fontWeight: 'bold',
+                                fontSize: labelFontSize,
+                            },
+                        },
+                    },
+                    {
+                        // M-Flare
+                        from: 0.00001,
+                        to: 0.0001,
+                        // zIndex: 5,
+                        color: 'rgba(0, 0, 0, 0)',
+                        label: {
+                            text: 'M',
+                            x: -45,
+                            style: {
+                                color: labelColor,
+                                fontWeight: 'bold',
+                                fontSize: labelFontSize,
+                            },
+                        },
+                    },
+                    {
+                        // X-Flare
+                        from: 0.0001,
+                        to: 1000000000,
+                        // zIndex: 5,
+                        color: 'rgba(40, 40, 40, 40)',
+                        label: {
+                            text: 'X',
+                            x: -45,
+                            style: {
+                                color: labelColor,
+                                fontWeight: 'bold',
+                                fontSize: labelFontSize,
+                            },
+                        },
+                    },
+                ],
             },
-            labels: {
-                x: -40,
-                style: {
-                    fontSize:labelFontSize
-                }
+            plotOptions: {
+                series: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    point: {
+                        events: {
+                            click() {
+                                alert(
+                                    'Date: ' +
+                                        Highcharts.dateFormat('%Y %m %d %H %M %S \n', this.x) +
+                                        'Value: ' +
+                                        this.y
+                                )
+                            },
+                        },
+                    },
+                    // lineWidth: 0.7,
+                },
             },
-            gridLineWidth: 0,
-            tickLength: 0,
-            plotBands: [
+            series: [
                 {
-                    // A-Flare
-                    from: 0,
-                    to: 0.0000001,
-                    // zIndex: 5,
-                    color: 'rgba(40, 40, 40, 40)',
-                    borderwidth: '100',
-                    borderColor: '#FFFF',
-                    label: {
-                        text: 'A',
-                        x: -20,
-                        style: {
-                            color: labelColor,
-                            fontWeight: 'bold',
-                            fontSize: labelFontSize,
-                        },
-                    },
-                },
-                {
-                    // B-Flare
-                    from: 0.0000001,
-                    to: 0.000001,
-                    // zIndex: 4,
-                    color: 'rgba(0, 0, 0, 0)',
-                    label: {
-                        text: 'B',
-                        x: -45,
-                        style: {
-                            color: labelColor,
-                            fontWeight: 'bold',
-                            fontSize: labelFontSize,
-                        },
-                    },
-                },
-                {
-                    // C-Flare
-                    from: 0.000001,
-                    to: 0.00001,
-                    // zIndex: 5,
-                    color: 'rgba(40, 40, 40, 40)',
-                    label: {
-                        text: 'C',
-                        x: -45,
-                        style: {
-                            color: labelColor,
-                            fontWeight: 'bold',
-                            fontSize: labelFontSize,
-                        },
-                    },
-                },
-                {
-                    // M-Flare
-                    from: 0.00001,
-                    to: 0.0001,
-                    // zIndex: 5,
-                    color: 'rgba(0, 0, 0, 0)',
-                    label: {
-                        text: 'M',
-                        x: -45,
-                        style: {
-                            color: labelColor,
-                            fontWeight: 'bold',
-                            fontSize: labelFontSize,
-                        },
-                    },
-                },
-                {
-                    // X-Flare
-                    from: 0.0001,
-                    to: 1000000000,
-                    // zIndex: 5,
-                    color: 'rgba(40, 40, 40, 40)',
-                    label: {
-                        text: 'X',
-                        x: -45,
-                        style: {
-                            color: labelColor,
-                            fontWeight: 'bold',
-                            fontSize: labelFontSize,
-                        },
-                    },
+                    showInLegend: false,
+                data
                 },
             ],
-        },
-        plotOptions: {
-            series: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                point: {
-                    events: {
-                        click() {
-                            alert('Date: ' + Highcharts.dateFormat('%Y %m %d %H %M %S \n', this.x) + 'Value: ' + this.y)
-                        },
-                    },
-                },
-                // lineWidth: 0.7,
-            },
-        },
-        series: [
-            {
-                showInLegend: false,
-                data: data
-            },
-        ],
-    }))
+        })
+    )
 }
 
 export default Chart
