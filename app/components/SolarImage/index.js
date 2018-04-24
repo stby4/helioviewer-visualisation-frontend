d40fd5cec705904e38bb054c981788c7ed45f196
Raphael Roussis <r.roussis@gmail.com>
2018-04-24 16:45:59 +0200

    current sun picture on start page
    Signed-off-by: Raphael Roussis <r.roussis@gmail.com>

--- app/components/SolarImage/index.js
+++ app/components/SolarImage/index.js
@@ -1,12 +1,14 @@
 import style from './style'
 
-//Modified Link (replace the existing img src):
-//<img src="https://legacy.helioviewer.org/api/v1/takeScreenshot/?imageScale=4&layers=[SDO,AIA,AIA,304,1,100]&events=&eventLabels=false&scale=false&scaleType=earth&scaleX=0&scaleY=0&date=${timestamp}&x1=-1300&x2=1300&y1=-1300&y2=1300&display=true&watermark=false" 
+//Modified Link for dynamic sun images:
+//full link with options: <img src="https://legacy.helioviewer.org/api/v1/takeScreenshot/?imageScale=4&layers=[SDO,AIA,AIA,304,1,100]&events=&eventLabels=false&scale=false&scaleType=earth&scaleX=0&scaleY=0&date=${timestamp}&x1=-1300&x2=1300&y1=-1300&y2=1300&display=true&watermark=false" 
+//optimized link: https://legacy.helioviewer.org/api/v1/takeScreenshot/?imageScale=1&layers=[SDO,AIA,AIA,171,10,100]&date=${timestamp}&x1=-1500&x2=1500&y1=-1500&y2=1500&display=true&watermark=false
+//Demo Sun image: 
+//<img src="./media/sun.png" 
 
-const SolarImagePreview = (timestamp, displayTime) =>
+const SolarImagePreview = (timeParam, timeDisplay) =>
 `<div id="${style.previewContainer}">
-<img src="./media/sun.png" 
-
-id="${style.solarImage}" alt="Solar Image Preview"><div class="${style.time}">${displayTime}</div><div><a class="${style.button}" href="https://helioviewer.org/?date=${timestamp}" target="_blank">View on helioviewer.org</a></div></div>`
+<img src="https://legacy.helioviewer.org/api/v1/takeScreenshot/?imageScale=1&layers=[SDO,AIA,AIA,171,10,100]&date=${timeParam}&x1=-1500&x2=1500&y1=-1500&y2=1500&display=true&watermark=false" 
+id="${style.solarImage}" alt="Solar Image Preview"><div class="${style.time}">${timeDisplay}</div><div><a class="${style.button}" href="https://helioviewer.org/?date=${timeParam}" target="_blank">View on helioviewer.org</a></div></div>`
 
 export default SolarImagePreview
--- app/index.js
+++ app/index.js
@@ -7,6 +7,18 @@ import TimelineMarkup, { Timeline } from 'components/Timeline'
 import 'theme/main.scss'
 
 document.getElementById('root').innerHTML = TimelineMarkup()
-document.getElementById('preview').innerHTML = SolarImagePreview('2014-02-25T15:53:00Z','2014/02/25 15:53:00 UTC' )
+
+//Format current date
+var timestamp = new Date()
+var year = timestamp.getFullYear()
+var month = ('0'+timestamp.getMonth()).substr(-2)
+var date = ('0'+timestamp.getDate()).substr(-2)
+var hours = ('0'+timestamp.getHours()).substr(-2)
+var minutes = ('0'+timestamp.getMinutes()).substr(-2)
+
+var timeParam = year + '-' + month + '-' + date + 'T' + hours  + ':' + minutes + ':00Z' 
+var timeDisplay = year + '/' + month + '/' + date + ' ' + hours  + ':' + minutes + ':00 UTC' 
+
+document.getElementById('preview').innerHTML = SolarImagePreview(timeParam, timeDisplay)
 
 Timeline()
