a99285d313b9472ac7ebf6b72d53116174ae0e52
Raphael Roussis <r.roussis@gmail.com>
2018-04-24 18:48:22 +0200

    Changed time format to UTC
    Signed-off-by: Raphael Roussis <r.roussis@gmail.com>

--- app/index.js
+++ app/index.js
@@ -9,12 +9,12 @@ import 'theme/main.scss'
 document.getElementById('root').innerHTML = TimelineMarkup()
 
 //Format current date
-var timestamp = new Date()
-var year = timestamp.getFullYear()
-var month = ('0'+timestamp.getMonth()).substr(-2)
-var date = ('0'+timestamp.getDate()).substr(-2)
-var hours = ('0'+timestamp.getHours()).substr(-2)
-var minutes = ('0'+timestamp.getMinutes()).substr(-2)
+var timestamp = new Date;
+var year = timestamp.getUTCFullYear()
+var month = ('0'+timestamp.getUTCMonth()).substr(-2)
+var date = ('0'+timestamp.getUTCDate()).substr(-2)
+var hours = ('0'+timestamp.getUTCHours()).substr(-2)
+var minutes = ('0'+timestamp.getUTCMinutes()).substr(-2)
 
 var timeParam = year + '-' + month + '-' + date + 'T' + hours  + ':' + minutes + ':00Z' 
 var timeDisplay = year + '/' + month + '/' + date + ' ' + hours  + ':' + minutes + ':00 UTC' 
