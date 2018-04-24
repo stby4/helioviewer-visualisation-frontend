94a54905dbf17ec1014bca64fa30dda2031d9042
Raphael Roussis <r.roussis@gmail.com>
2018-04-25 00:45:06 +0200

    SolarImage presentation/visualisation optimized
    Signed-off-by: Raphael Roussis <r.roussis@gmail.com>

--- app/components/SolarImage/index.js
+++ app/components/SolarImage/index.js
@@ -8,7 +8,7 @@ import style from './style'
 
 const SolarImagePreview = (timeParam, timeDisplay) =>
 `<div id="${style.previewContainer}">
-<img src="https://legacy.helioviewer.org/api/v1/takeScreenshot/?imageScale=1&layers=[SDO,AIA,AIA,171,10,100]&date=${timeParam}&x1=-1500&x2=1500&y1=-1500&y2=1500&display=true&watermark=false" 
+<img src="https://legacy.helioviewer.org/api/v1/takeScreenshot/?imageScale=1&layers=[SDO,AIA,AIA,171,10,100]&date=${timeParam}&x1=-1600&x2=1600&y1=-1250&y2=1300&display=true&watermark=false" 
 id="${style.solarImage}" alt="Solar Image Preview"><div class="${style.time}">${timeDisplay}</div><div><a class="${style.button}" href="https://helioviewer.org/?date=${timeParam}" target="_blank">View on helioviewer.org</a></div></div>`
 
 export default SolarImagePreview
